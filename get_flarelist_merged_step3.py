05cca1092321c95bba4547f632ae8b2e7ed0aebf
hayesla <hayesla@tcd.ie>
2023-08-07 11:35:48 +0200

    final flarelist merging

--- get_flarelist_merged_step3.py
+++ get_flarelist_merged_step3.py
@@ -5,76 +5,102 @@ from astropy.coordinates import SkyCoord
 from astropy.time import Time
 import astrospice
 from sunpy.coordinates import frames
-import time
 import sunpy.map
+
+from flarelist_fns import check_bp_maps
+from flarelist_coord_utils import is_visible
+
 
 flarelist_step1 = pd.read_csv("stix_flarelist_with_files_1000_for_idlinput.csv")
 idl_output_step2 = pd.read_csv("flarelist_locations_idloutput_416keV_1000counts_40s.csv")
 
 full_flarelist = pd.merge(flarelist_step1, idl_output_step2,  how='inner', on="flare_id")
 
-
-
-def check_bp_maps(i, rootdir='./bp_maps_416', plot=False):
-    """
-    Function to check the back-projection maps generated through the location estimatation. 
-    Here, this is done by checking the fits file of the map, and then removing the pixels surrounding 
-    the brightest pixel and then checkign if the next brightest pixel is 90% of the max. 
-    If it is, then we say that its not a good "map", and that the value of the flare location 
-    isn't trustworthy. 
-
-    Here, you pass the index of the `full_flarelist` dataframe.
-
-    Parameters:
-    ----------
-    i : int, 
-        index of the `full_flarelist` dataframe
-    root_dir : `str`, directory of where map fits are saved
-        default to `./bp_maps416`
-    plot : Boolean, optional
-        If True, then plots the map with the brightest pixel as a cross and shows the
-        marked out pixels
-
-    Returns:
-    --------
-    `str` : `pass` or `fail` 
-
-    """
-    file = rootdir+'/bp_nat_map-{:d}_{:d}.fits'.format(full_flarelist["req_id"].iloc[i],
-                                                       full_flarelist["flare_id"].iloc[i])
-    map_test = sunpy.map.Map(file)
-    map_max_val = np.max(map_test.data)
-    x, y = np.where(map_test.data == map_max_val)
-    new_data = map_test.data.copy()
-    new_data[x[0]-20:x[0]+20, y[0]-20:y[0]+20]=0
-    
-    new_max_val = np.max(new_data)
-    if new_max_val < 0.9*map_max_val:
-        confidence = "pass"
-
-    else:
-        confidence = "fail"
-    
-    if plot:
-        new_map = sunpy.map.Map(new_data, map_test.meta)
-        fig = plt.figure(figsize=(9, 4))
-        ax1 = fig.add_subplot(1, 2, 1, projection=map_test)
-        ax2 = fig.add_subplot(1, 2, 2, projection=new_map)
-        map_test.plot(axes=ax1, vmin=0, vmax=map_max_val, cmap="viridis")
-        map_test.draw_limb(axes=ax1)
-        new_map.plot(axes=ax2, vmin=0, vmax=map_max_val, cmap="viridis")
-        new_map.draw_limb(axes=ax2)
-    
-    return confidence
-
-t1 = time.time()
+# Get the confidence of the back projection maps output
 confidence = []
 for i in range(len(full_flarelist)):
-    confidence.append(check_bp_maps(i))
+    confidence.append(check_bp_maps(full_flarelist, i))
 
-t2 = time.time()
-print(t2-t1)
 full_flarelist["confidence"] = confidence
 
+# Drop rows that don't pass the back projection test
+flarelist = full_flarelist[full_flarelist["confidence"]=="pass"]
+flarelist.reset_index(inplace=True, drop=True)
+flarelist.drop(columns="confidence", inplace=True)
 
-full_flarelist.to_csv("full_flarelist_with_locations_1000_416keV.csv", index=False, index_label=False)
+#full_flarelist.to_csv("full_flarelist_with_locations_1000_416keV.csv", index=False, index_label=False)
+# get the position of Solar Orbiter as a function of the flare times
+kernals = astrospice.registry.get_kernels("solar orbiter", "predict")
+solo_coords_full = astrospice.generate_coords("SOLAR ORBITER", pd.to_datetime(flarelist["peak_UTC"])).heliographic_stonyhurst
+earth_coords_full = astrospice.generate_coords("earth", pd.to_datetime(flarelist["peak_UTC"])).heliographic_stonyhurst
+
+# make new columns for Solar Orbiter lat, lon
+flarelist.loc[:, "solo_position_lat"] = solo_coords_full.lat.value
+flarelist.loc[:, "solo_position_lon"] = solo_coords_full.lon.value
+flarelist.loc[:, "solo_position_AU_distance"] = solo_coords_full.radius.to_value(u.AU)
+
+# create SkyCoord objects of the X, Y arcsec from Solar Orbiter observer
+flare_coords_solo_hpc = SkyCoord(flarelist["X_arcsec"].values*u.arcsec, 
+                                 flarelist["Y_arcsec"].values*u.arcsec, 
+                                 frame=frames.Helioprojective(observer=solo_coords_full))
+
+# Here we're are transforming these coordinates to HPC from earth, HGS and HGC. 
+# For events off limb we are assuming a spherical screen to deal with the reprojection.
+with frames.Helioprojective.assume_spherical_screen(flare_coords_solo_hpc.observer, only_off_disk=True):
+    flare_coords_earth_hpc = flare_coords_solo_hpc.transform_to(frames.Helioprojective(observer=earth_coords_full))
+    flare_coords_hgs = flare_coords_solo_hpc.transform_to(frames.HeliographicStonyhurst)
+    flare_coords_hgc = flare_coords_solo_hpc.transform_to(frames.HeliographicCarrington)
+
+# Now, the HPC coordinates at Earth might be on the backside of the Sun - its gives you teh X, Y as 
+# though you "see" through the Sun. Here we check if these coordinates are actually visible from 
+# an Earth observer
+visible_frame_earth = is_visible(flare_coords_earth_hpc)
+flarelist.loc[:, "visible_from_earth"] = visible_frame_earth
+
+flarelist.rename(columns={'LC0_PEAK_COUNTS_4S': '4-10 keV', 
+                          'LC1_PEAK_COUNTS_4S': "10-15 keV",
+                          'LC2_PEAK_COUNTS_4S': "15-25 keV", 
+                          'LC3_PEAK_COUNTS_4S': "25-50 keV", 
+                          'LC4_PEAK_COUNTS_4S': "50-84 keV", 
+                          'LC0_BKG' : 'bkg_baseline_4-10 keV',
+                          'LC0_BKG_COUNTS_4S': 'bkg 4-10 keV', 
+                          'LC1_BKG_COUNTS_4S': "bkg 10-15 keV",
+                          'LC2_BKG_COUNTS_4S': "bkg 15-25 keV", 
+                          'LC3_BKG_COUNTS_4S': "bkg 25-50 keV", 
+                          'LC4_BKG_COUNTS_4S': "bkg 50-84 keV", 
+                          'req_id' : 'file_request_id_used',
+                          'timeshift' : 'light_travel_time',
+                          'GOES_class' : 'GOES_class_time_of_flare',
+                          'GOES_flux' : 'GOES_flux_time_of_flare',
+                          'X_arcsec' : 'hpc_x_solo',
+                          'Y_arcsec' : 'hpc_y_solo',
+                          }, inplace=True)
+
+
+flarelist.loc[:, 'hpc_x_earth'] = flare_coords_earth_hpc.Tx.value
+flarelist.loc[:, 'hpc_y_earth'] = flare_coords_earth_hpc.Ty.value
+
+# set X, Y HPC earth to nan if not visible from earth
+flarelist.loc[flarelist['visible_from_earth'] == False, ['hpc_x_earth', 'hpc_y_earth']] = np.nan
+
+flarelist.loc[:, 'hgs_lon'] = flare_coords_hgs.lon.value
+flarelist.loc[:, 'hgs_lat'] = flare_coords_hgs.lat.value
+
+flarelist.loc[:, 'hgc_lon'] = flare_coords_hgc.lon.value
+flarelist.loc[:, 'hgc_lat'] = flare_coords_hgc.lat.value
+
+
+columns = ['start_UTC', 'end_UTC', 'peak_UTC', '4-10 keV', '10-15 keV', '10-15 keV', '15-25 keV', '25-50 keV', '50-84 keV',
+           'att_in',
+           'bkg 4-10 keV', 'bkg 10-15 keV', 'bkg 15-25 keV', 'bkg 25-50 keV', 'bkg 50-84 keV', 'bkg_baseline_4-10 keV',
+           'hpc_x_solo', 'hpc_y_solo', 'hpc_x_earth', 'hpc_y_earth', 'visible_from_earth', 
+           'hgs_lon', 'hgs_lat', 'hgc_lon', 'hgc_lat', 
+           'solo_position_lat', 'solo_position_lon', 'solo_position_AU_distance', 'light_travel_time',
+           'file_request_id_used', 'GOES_class_time_of_flare', 'GOES_flux_time_of_flare', 'flare_id']
+
+
+
+final_flarelist = flarelist[columns]
+final_flarelist.to_csv("STIX_flarelist_w_locations_{:s}_{:s}_version1.csv".format(pd.to_datetime(final_flarelist["start_UTC"].min()).strftime("%Y%m%d"), 
+                                                                                  pd.to_datetime(final_flarelist["start_UTC"].max()).strftime("%Y%m%d")), 
+                       index=False, index_label=False)
